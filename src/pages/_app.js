cd28b0a705df6b7474ca573ac7fb0638d12dc2c7
danielrichardson1 <drich1475@gmail.com>
2024-12-01 21:55:55 -0500

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "@/styles/globals.scss";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return 
+  <main className={montserrat.className}>
+      <Component {...pageProps} />;
+  </main>
+
 }
